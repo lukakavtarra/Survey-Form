c9d71de2e1dfe373f5247d89ae73459c513cdb66
Luka <65070584+lukakavtarra@users.noreply.github.com>
2022-03-04 14:54:26 +0400

    hiding date input
    add js file for hiding date input and correcting mistakes

---
+++ javascript/dateinput.js
@@ -0,0 +1,21 @@
+function vaccineDateHide() {
+    var n = document.getElementById("vaccine-date");
+    n.style.display = "none"
+
+}
+
+function vaccineDateShow() {
+    var b = document.getElementById("vaccine-date");
+    b.style.display = "block"
+}
+
+function covidDatehide() {
+    var n = document.getElementById("covid-date");
+    n.style.display = "none"
+
+}
+
+function covidDateShow() {
+    var b = document.getElementById("covid-date");
+    b.style.display = "block"
+}
